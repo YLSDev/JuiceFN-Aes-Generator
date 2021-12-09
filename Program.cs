ee5bb20e3826f58faceebef1da780342248a0cf7
YLSDev <41091863+YLSDev@users.noreply.github.com>
2021-12-09 20:54:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net;
 
 WebClient aesGrabberClient = new WebClient();
-string aesKey = aesGrabberClient.DownloadString("https://benbot.app/api/v1/aes");
-Console.WriteLine("The generated AES key is: " + aesKey);
+string AESKey = aesGrabberClient.DownloadString("https://benbot.app/api/v1/aes");
+Console.WriteLine("The generated AES key is: " + AESKey);
 Console.WriteLine("USE CODE JUICEFN THIS TOOK ME ALOT OF TIME");
